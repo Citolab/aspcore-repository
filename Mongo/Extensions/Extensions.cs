42cec0fe44dfab1b8fa6cd66ea8505463a421de7
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2018-01-29 19:59:03 +0100

    fixed missing IRepositoryOptions error

--- Mongo/Extensions/Extensions.cs
+++ Mongo/Extensions/Extensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Citolab.Repository.Helpers;
 using Citolab.Repository.Mongo.Options;
-using Citolab.Repository.NoAction;
+using Citolab.Repository.Options;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Newtonsoft.Json;
@@ -15,20 +15,11 @@ namespace Citolab.Repository.Mongo.Extensions
             if (services == null) throw new ArgumentNullException(nameof(services));
             services.AddMemoryCache();
             services.AddLogging();
-            services.AddSingleton(new MongoDatabaseOptions(databaseName, connectionString));
+            services.AddSingleton<IRepositoryOptions>(new MongoDatabaseOptions(databaseName, connectionString));
             // TryAdd will only add if there isn't yet a ILoggedInUserProvider registered.
             // if someone wants to use its own ILoggedInUserProvider, it should be added before calling this function.
             services.TryAddScoped<ILoggedInUserProvider, NoLoggedInUser>();
             services.AddSingleton<IRepositoryFactory, MongoFactory>();
-        }
-
-        /// <summary>
-        ///     Deep clone using JSON serialization.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="toClone"></param>
-        /// <returns></returns>
-        public static T Clone<T>(this T toClone) where T : class =>
-            JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(toClone));        
+        }     
     }
 }
--- SqlServer/Extensions/Extensions.cs
+++ SqlServer/Extensions/Extensions.cs
@@ -17,7 +17,7 @@ namespace Citolab.Repository.Extensions
             if (services == null) throw new ArgumentNullException(nameof(services));
             services.AddMemoryCache();
             services.AddLogging();
-            services.AddSingleton(new SqlServerDatabaseOptions(databaseName, connectionString));
+            services.AddSingleton<IRepositoryOptions>(new SqlServerDatabaseOptions(databaseName, connectionString));
             // TryAdd will only add if there isn't yet a ILoggedInUserProvider registered.
             // if someone wants to use its own ILoggedInUserProvider, it should be added before calling this function.
             services.TryAddScoped<ILoggedInUserProvider, NoLoggedInUser>();
