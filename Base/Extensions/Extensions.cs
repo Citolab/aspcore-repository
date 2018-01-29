1bd21f365f9f860e5f076566e6d41eae7e040cd5
Marcel Hoekstra <marcel.hoekstra@cito.nl>
2018-01-29 09:15:32 +0100

    changed nuspec

--- Base/Extensions/Extensions.cs
+++ Base/Extensions/Extensions.cs
@@ -2,7 +2,6 @@
 using Citolab.Repository.Helpers;
 using Citolab.Repository.NoAction;
 using Citolab.Repository.Options;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Newtonsoft.Json;
