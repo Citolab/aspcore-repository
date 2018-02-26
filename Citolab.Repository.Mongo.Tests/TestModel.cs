875544a33801b527efd3ed0761d48f3bb938955f
Tjeerd Hans Terpstra <tjeerdhans.terpstra@cito.nl>
2018-02-26 12:50:42 +0100

    Added unit test

---
+++ Citolab.Repository.Mongo.Tests/TestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Citolab.Repository.Mongo.Tests
+{
+    public class TestModel : Model
+    {
+        public string Name { get; set; }
+    }
+}
